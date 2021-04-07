bc5be66640e42e0c02bb3c867c7d00229990f0ac
DAN <djmartinezsuarez91@gmail.com>
2021-04-07 15:08:51 +0200

    Task 008: Done.

--- src/main/java/acme/features/authenticated/task/AuthenticatedTaskRepository.java
+++ src/main/java/acme/features/authenticated/task/AuthenticatedTaskRepository.java
@@ -1,5 +1,5 @@
 
-package acme.features.anonymous.task;
+package acme.features.authenticated.task;
 
 import java.util.Collection;
 
@@ -10,13 +10,16 @@ import acme.entities.tasks.Task;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
-public interface AnonymousTaskRepository extends AbstractRepository {
+public interface AuthenticatedTaskRepository extends AbstractRepository {
 
 	@Query("select t from Task t")
 	Collection<Task> findMany();
 
 	@Query("select t from Task t where CURRENT_TIMESTAMP >= t.executionStart and CURRENT_TIMESTAMP < t.executionEnd order by t.workLoad DESC")
 	Collection<Task> findManyActives();
+	
+	@Query("select t from Task t where CURRENT_TIMESTAMP >= t.executionEnd order by t.workLoad DESC")
+	Collection<Task> findManyFinished();
 	
 	@Query("select t from Task t where t.id = ?1")
 	Task findOneById(int id);
